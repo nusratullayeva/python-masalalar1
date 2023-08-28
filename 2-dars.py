1cf30e591eb56a5b0047bf99d946fb2a3edfc8bc
nusratllayeva <anvarsaidmurotov@gmail.com>
2023-08-28 18:00:20 +0500

    master

---
+++ 2-dars.py
@@ -0,0 +1,23 @@
+# 1-masala
+nameInput = input('Ismingizni kiriting: ')
+surnameInput = input('Familiyangizni kiriting: ')
+yearInput = int(input('Yilingizni kiriting: '))
+print(f'Mening ismim {nameInput}, familiyam {surnameInput}, yoshim {2023 - yearInput} da!')
+
+
+
+
+# 2-masala
+inputNum = int(input('Kiriting: '))
+inputNum2 = inputNum**2
+print(inputNum2)
+
+
+
+
+# 3-masala
+inputNum3 = int(input('a ni Kiriting: '))
+inputNum4 = int(input('b ni Kiriting: '))
+resultInputs = inputNum3 * inputNum4
+resultInputs2 = 2 * (resultInputs)
+print(resultInputs2)
